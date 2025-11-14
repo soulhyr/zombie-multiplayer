6ea6255fe493f03b5ca6e145af36d754ff61fce8
Richard <31912105+soulhyr@users.noreply.github.com>
2025-11-14 11:00:45 +0900

    로비 룸 목록 오류 수정
    로비 룸 목록 오류 수정

--- ZombieMultiplayer/Assets/Scripts/UIRoomScollview.cs
+++ ZombieMultiplayer/Assets/Scripts/UIRoomScollview.cs
@@ -15,35 +15,59 @@ public class UIRoomScollview : MonoBehaviour
 
     public void UpdateUI(List<RoomInfo> roomList)
     {
-        for (int i = 0; i < content.childCount; i++)
-            Destroy(content.GetChild(i).gameObject);
-
-        Debug.Log(content.childCount);
         foreach (var room in roomList)
         {
             Debug.Log(room);
             string id = room.Name;
             int max = room.MaxPlayers;
             string roomName = DataManager.Instance.AddRoom(id, max);
+            Transform child = null;
+            for (int i = 0; i < content.childCount; i++)
+            {
+                if (content.GetChild(i).name == id)
+                {
+                    Debug.Log("search:" + content.GetChild(i).name);
+                    child = content.GetChild(i);
+                    break;
+                }
+            }
+            
             if (room.RemovedFromList || room.PlayerCount == 0)
             {
-                // Debug.Log($"룸 아이템 삭제!, room.Name: {room.Name}");
+                if (child != null)
+                {
+                    Debug.Log("delete object");
+                    DestroyImmediate(child.gameObject);
+                }
+
+                Debug.Log("delete data");
                 DataManager.Instance.RemoveRoom(id);
                 continue;
             }
-            Debug.Log("생성");
-            var item = Instantiate(uiRoomCellViewPrefab, content);
-            item.name = id;
-            UIRoomCellView itemLogic = item.GetComponent<UIRoomCellView>();
-            itemLogic.roomInfo.id = id;
-            itemLogic.roomInfo.roomName = roomName;
-            itemLogic.roomInfo.maxPlayers = max;
-            itemLogic.roomInfo.currentPlayers = 1;
-            itemLogic.SetRoomName();
+
+            if (child != null)
+            {
+                Debug.Log("갱신");
+                UIRoomCellView logic = child.GetComponent<UIRoomCellView>();
+                logic.roomInfo.maxPlayers = max;
+                logic.roomInfo.currentPlayers = room.PlayerCount;
+                logic.SetRoomName();
+            }
+            else
+            {
+                Debug.Log("생성");
+                var item = Instantiate(uiRoomCellViewPrefab, content);
+                item.name = id;
+                UIRoomCellView logic = item.GetComponent<UIRoomCellView>();
+                logic.roomInfo.id = id;
+                logic.roomInfo.roomName = roomName;
+                logic.roomInfo.maxPlayers = max;
+                logic.roomInfo.currentPlayers = 1;
+                logic.SetRoomName();
+            }
         }
-        Debug.Log(content.childCount);
+        
         txtNoRoom.SetActive(content.childCount == 0);
-        // Debug.Log($"실제 보이는 룸 수 : {content.childCount}");
     }
 
     public void Show() => gameObject.SetActive(true);
